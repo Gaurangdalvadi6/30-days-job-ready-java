73b11473f6488ecd92ca4bb8c385a122c0afc346
gaurangdalvadi6 <gaurangdalvadi6@gmail.com>
2026-01-17 23:28:56 +0530

    Day 10 added programs

---
+++ Day 10 Basic Algorithms/src/twoDArray/SearchInSortedMatrix.java
@@ -0,0 +1,37 @@
+package twoDArray;
+
+public class SearchInSortedMatrix {
+
+    public static int[] stairCaseSearch(int[][] matrix,int key){
+        int row=0,col= matrix[0].length-1;
+
+        while (row< matrix.length && col>=0){
+            if (matrix[row][col]==key){
+//                System.out.println("found key at ("+ row+","+col+")");
+                return new int[]{row,col};
+            } else if (key< matrix[row][col]) {
+                col--;
+            }
+            else {
+                row++;
+            }
+        }
+//        System.out.println("key not found");
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{10,20,30,40},
+                {15,25,35,45},
+                {27,29,37,48},
+                {32,33,39,50}};
+
+        int key = 40;
+        int[] stairCaseSearch = stairCaseSearch(matrix, key);
+        if (stairCaseSearch != null) {
+            System.out.println("Position: [" + stairCaseSearch[0] + "][" + stairCaseSearch[1] + "]");
+        }
+        else
+            System.out.println("Not Found");
+    }
+}
